ea0f031fab98fb22c47536b866dd3295a0a9e68e
yaswanth2813 <109852735+yaswanth2813@users.noreply.github.com>
2023-11-24 18:14:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function day() {
+    document.getElementById("projects").style.backgroundColor = "white";
+}
+
+function dark() {
+    document.getElementById("projects").style.backgroundColor = "black";
+}
